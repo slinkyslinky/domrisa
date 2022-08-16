8b7d43acae8a6caffebb4dd40bb5cf6a4e494f40
SlinkySlinky <slinkislinki@gmail.com>
2022-08-16 23:33:23 +0300

    added review screen

--- Documents/.Programming/Lisa/src/components/reviews/Reviews.jsx
+++ Documents/.Programming/Lisa/src/components/reviews/Reviews.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import './reviews.scss'
-import ContactForm from '../contactForm/ContactForm'
+import review1 from '../../img/review1.jpg';
+import ReviewsItem from '../reviewsItem/ReviewsItem';
 
 export default function Reviews() {
+
+   const reviewList = [
+      { id: 1, date: '29 июля', image: review1, review: '', reviewsPhoto: '', answer: '', answerPhoto: '' },
+   ]
+
+
    return (
       <div className='reviews'>
-         <ContactForm />
+         <div className="reviews__wrapper">
+            <h2>Отзывы</h2>
+            <div className="reviews__content">
+               {reviewList.map(review =>
+                  <ReviewsItem
+                     key={review.id}
+                     date={review.date}
+                     image={review.image}
+                     review={review.review}
+                     reviewsPhoto={review.reviewsPhoto}
+                     answer={review.answer}
+                     answerPhoto={review.answerPhoto}
+                  />
+               )}
+            </div>
+         </div>
       </div>
    )
 }
